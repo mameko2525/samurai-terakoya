7950ca653a67da17a8180d6870b68892e8f19477
mameko2525 <142704230+mameko2525@users.noreply.github.com>
2023-08-21 19:27:03 +0900

    Add files via upload

---
+++ java-basic-kadai/Score_Chapter04.java
@@ -0,0 +1,24 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		System.out.println("Aさん10点");
+		System.out.println("Bさん20点");
+		System.out.println("Cさん30点");
+		System.out.println("Dさん40点");
+		System.out.println("Eさん50点");
+		System.out.println("Fさん60点");
+		System.out.println("Gさん70点");
+		System.out.println("Hさん80点");
+		System.out.println("Iさん90点");
+		System.out.println("Jさん100点");
+		
+		System.out.println("10人の平均点は" + (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10 +"点");
+		
+		
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
